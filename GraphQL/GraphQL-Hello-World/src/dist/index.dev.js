6317d7aafa6775b1f490edab4424750622f7ccc6
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-23 18:19:58 +0800

    GraphQL Basic Types

--- GraphQL/GraphQL-Hello-World/src/dist/index.dev.js
+++ GraphQL/GraphQL-Hello-World/src/dist/index.dev.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n    type Query {\n        hello: String!\n     }  \n"]);
+  var data = _taggedTemplateLiteral(["\n    type Query {\n        hello: String\n        user: User\n    }  \n\n    type User {\n        id: ID!\n        username: String!\n    }\n    \n    type Error {\n        field: String!\n        message: String!\n    }\n\n    type RegisterResponse {\n        errors: [Error!]!\n        user: User\n    }\n\n    input UserInfo {\n        username: String!, \n        password: String!, \n        age: Int\n    }\n\n    type Mutation {\n        register(userInfo: UserInfo!): RegisterResponse!\n        login(userInfo: UserInfo!): Boolean!\n    }\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -15,13 +15,40 @@ function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(
 var _require = require('apollo-server'),
     ApolloServer = _require.ApolloServer,
     gql = _require.gql; // tanda ! menandakan bahwa tidak boleh null 
+// tanda ! didalam array mendakan bahwa array tidak boleh null
 
 
 var typeDefs = gql(_templateObject());
 var resolvers = {
   Query: {
     hello: function hello() {
-      return "Hello World!";
+      return null;
+    },
+    user: function user() {
+      return {
+        id: 1,
+        username: "mufrad"
+      };
+    }
+  },
+  Mutation: {
+    login: function login() {
+      return true;
+    },
+    register: function register() {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'bad'
+        }, {
+          field: 'username2',
+          message: 'bad2'
+        }],
+        user: {
+          id: 1,
+          username: "mufrad"
+        }
+      };
     }
   }
 };
--- GraphQL/GraphQL-Hello-World/src/index.js
+++ GraphQL/GraphQL-Hello-World/src/index.js
@@ -1,14 +1,67 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 // tanda ! menandakan bahwa tidak boleh null 
+// tanda ! didalam array mendakan bahwa array tidak boleh null
 const typeDefs = gql`
     type Query {
-        hello: String!
-     }  
+        hello: String
+        user: User
+    }  
+
+    type User {
+        id: ID!
+        username: String!
+    }
+    
+    type Error {
+        field: String!
+        message: String!
+    }
+
+    type RegisterResponse {
+        errors: [Error!]!
+        user: User
+    }
+
+    input UserInfo {
+        username: String!, 
+        password: String!, 
+        age: Int
+    }
+
+    type Mutation {
+        register(userInfo: UserInfo!): RegisterResponse!
+        login(userInfo: UserInfo!): Boolean!
+    }
 `;
+
 const resolvers = {
     Query: {
-        hello: () => "Hello World!"
+        hello: () => null,
+        user: () => ({
+            id: 1,
+            username: "mufrad"
+        })
+    },
+    Mutation: {
+        login: () => true,
+        register: () =>({
+            errors: [
+            {
+                field: 'username',
+                message: 'bad'
+            },
+            {
+                field: 'username2',
+                message: 'bad2'
+                
+            }
+            ],
+            user: {
+                id: 1,
+                username: "mufrad"
+            }
+        })
     }
 }
 
